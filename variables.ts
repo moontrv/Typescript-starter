447b77f7cae16e7bb787a7824d887e72ccb1fb65
Minh N <pesrobot@gmail.com>
2019-08-19 07:03:30 +0300

    starting

---
+++ variables.ts
@@ -0,0 +1,16 @@
+let apples: number = 5;
+let speed: string = "test";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
+
+let colors: string[] = ["red", "yellow", "purple"];
+let myNumbers: number[] = [12, 343, 4];
+let true_th: boolean[] = [true, false, false];
+
+class Car {}
+
+let car: Car = new Car();
